b1a628ae4de0b2aeed8a3716485d3a99df5667f2
swlee <swo98@ajou.ac.kr>
2025-07-13 17:06:10 +0900

    fix: orderItemRepository extends 잘못하던거 수정

--- src/main/java/lab/jpa_fetchjoin_paging/domain/repository/item/OrderItemRepository.java
+++ src/main/java/lab/jpa_fetchjoin_paging/domain/repository/item/OrderItemRepository.java
@@ -1,10 +1,9 @@
 package lab.jpa_fetchjoin_paging.domain.repository.item;
 
 import lab.jpa_fetchjoin_paging.domain.entity.OrderItemEntity;
-import lab.jpa_fetchjoin_paging.domain.repository.order.CustomOrderRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderItemRepository extends JpaRepository<OrderItemEntity, Long>, CustomOrderRepository {
+public interface OrderItemRepository extends JpaRepository<OrderItemEntity, Long>, CustomOrderItemRepository {
 }
